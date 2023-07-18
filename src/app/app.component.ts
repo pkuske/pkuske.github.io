0f864555db62bd7db42863a1dc49c34449aa7898
pkuske <pkuske@live.de>
2023-07-18 17:36:52 +0200

    redo of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PostMessageService } from './post-message.service';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +6,8 @@ import { PostMessageService } from './post-message.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'postmessage-test';
-  showDiv = false;
-
-  constructor(private postMessageService: PostMessageService) {}
-
-  testPostMessage() {
-    const messageData = {
-      type: 'MESSAGE_TYPE',
-      payload: 'Hello from Angular!',
-    };
-    window.parent.postMessage(JSON.stringify(messageData));
-    // const postMessageData = { type: 'SUCCESS' };
-    // this.postMessageService.postMessage(postMessageData);
-    // // console.log('postMessage send');
-    // // window.parent.postMessage(JSON.stringify({ type: 'SUCCESS' }));
-    // this.showDiv = !this.showDiv;
+  sendMessage() {
+    // Send a message to the parent window (React app)
+    window.parent.postMessage('Hello from Angular!', '*');
   }
 }
