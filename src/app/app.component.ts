8c1de8033355c977ca240a4b7791f2dc59cb0975
Pascal Kuske <pkuske@live.de>
2023-07-18 16:04:02 +0200

    added test div to show functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'postmessage-test';
+  showDiv = false;
 
   testPostMessage() {
-    console.log("postMessage send");
+    console.log('postMessage send');
     window.parent.postMessage('SUCCESS', '*');
+    this.showDiv = !this.showDiv;
   }
 }
